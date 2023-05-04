31b3c5ba9c22599e100d5b302dd6d38cb33b0fdd
Bayan Banat <125543324+BayanBanat@users.noreply.github.com>
2023-05-04 15:37:47 +0300

    Update code_challenge1.py

--- Python/code_challenge1/code_challenge1.py
+++ Python/code_challenge1/code_challenge1.py
@@ -1,1 +1,10 @@
 
+def reverseArray(arr):
+    reverseArr = []
+    i = len(arr) - 1
+    while i >= 0:
+        reverseArr.append(arr[i])
+        i -= 1
+    return reverseArr
+
+print(reverseArray([1, 2, 3, 4, 5, 6]))
